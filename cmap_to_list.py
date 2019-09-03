7d98c187f7fc667c3b6688e6daac66d52846838f
constance-scherer <47219113+constance-scherer@users.noreply.github.com>
2019-09-03 13:39:31 +0200

    learning how to write code
    learning the use of the touche entrée smh

--- cmap_to_list.py
+++ cmap_to_list.py
@@ -3,5 +3,6 @@ def cmap_to_list(cmap_name, taille) :
 	cmap = matplotlib.cm.get_cmap(cmap_name) 
 	liste = [] 
 	for i in range(0, taille) : 
-		n = i/taille liste.append(cmap(n)) 
+		n = i/taille 
+		liste.append(cmap(n)) 
 	return liste
